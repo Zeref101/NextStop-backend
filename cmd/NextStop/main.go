9486e419a8a6872aaaa642f7e958215b3bdf2ce7
Zeref101 <shreyas.mohanty03@gmail.com>
2024-07-03 22:20:34 +0530

    chore: Add CORS configuration and proxy config

--- cmd/NextStop/main.go
+++ cmd/NextStop/main.go
@@ -2,17 +2,38 @@ package main
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/Zeref101/internal/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
+func greetings(ctx *gin.Context) {
+    ctx.JSON(http.StatusOK, gin.H{
+        "Hello": "visitor",
+    })
+    return
+}
+
 func main() {
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context){
-		c.JSON(http.StatusOK, gin.H{
-			"message":"hallllooo",
-		})
-	})
 
-	r.Run(":5000")
+	if err := r.SetTrustedProxies(nil); err!=nil{
+		panic(err)
+	}
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"GET","POST","PUT","DELETE"},
+		AllowHeaders: []string{"Origin"},
+		ExposeHeaders: []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge: 12 * time.Hour,
+	}))
+	routes.PlacesInfoRoutes(r)
+
+	r.POST("/", greetings)
+
+	r.Run(":7000")
 }
