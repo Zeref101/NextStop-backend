057f44c503264efadd0ae53ce0e030b9df13c0a5
Zeref101 <shreyas.mohanty03@gmail.com>
2024-07-03 21:24:21 +0530

    feat: /placesInfo/place created which gets the places info from web scraper

--- internal/services/get_places_result.go
+++ internal/services/get_places_result.go
@@ -1,5 +1,33 @@
-package main
+package services
 
-func main() {
-	
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type PlacesProps struct {
+	Title string `json:"title"`
+	ImgURL string `json:"img_url"`
+	Description string `json:"description"`
+}
+
+func GetPlaces(c *gin.Context){
+    var places_info []PlacesProps
+    
+    if err := c.ShouldBindJSON(&places_info); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+
+    fmt.Println(places_info)
+    c.JSON(http.StatusOK, gin.H{
+        "places_info": places_info,
+    })
+}
+func GreetingPlaces(c *gin.Context){
+    c.JSON(http.StatusOK, gin.H{
+        "message":"/places",
+    })
 }
