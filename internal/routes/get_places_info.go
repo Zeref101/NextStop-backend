c3f7b266120a457098bc971017a9b4fd35dcf0fa
Zeref101 <shreyas.mohanty03@gmail.com>
2024-07-23 12:57:06 +0530

    feat: Add GET route for placesInfo

--- internal/routes/get_places_info.go
+++ internal/routes/get_places_info.go
@@ -8,6 +8,7 @@ import (
 func PlacesInfoRoutes(router *gin.Engine){
 	placesInfo := router.Group("/placesInfo")
 	{
+		placesInfo.GET("/",services.GreetingPlaces)
 		placesInfo.POST("/place", services.GetPlaces)
 	}
 }
