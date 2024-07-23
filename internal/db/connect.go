06f0b9666345321b90287f0477564b6d5103fbf8
Zeref101 <shreyas.mohanty03@gmail.com>
2024-07-23 12:56:55 +0530

    chore: Refactor database connection code to use gorm

--- internal/db/connect.go
+++ internal/db/connect.go
@@ -1,46 +1,52 @@
 package db
 
 import (
+	"fmt"
 	"log"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-var db *gorm.DB
+var DB *gorm.DB
 
-func ConnectToDB(){
+func ConnectToDB() (*gorm.DB, error){
 
 	dsn := "user=postgres.mianfanfnhpopqdbbdzi password=thedarkslayer@59 dbname=postgres sslmode=require host=aws-0-ap-south-1.pooler.supabase.com port=6543"
 
 	var err error
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
-	if err != nil {
-		log.Fatal("Failed to connect to db: ", err)
-	}
+    if err != nil {
+        return nil, fmt.Errorf("failed to connect to db: %w", err)
+    }
 
     sqlDB, err := db.DB()
     if err != nil {
-        log.Fatal("Failed to get database connection:", err)
+        return nil, fmt.Errorf("failed to get database connection: %w", err)
     }
 
     err = sqlDB.Ping()
     if err != nil {
-        log.Fatal("Failed to ping database:", err)
+        return nil, fmt.Errorf("failed to ping database: %w", err)
     }
 
     log.Println("Database connection successfully established")
 
+	return db, nil
+
 }
 
-func CloseDB(){
+func CloseDB(dbInstance *gorm.DB){
 
-	sqlDB, err := db.DB()
+
+	sqlDB, err := dbInstance.DB()
 
 	if err != nil{
 		log.Fatal("Failed to close db connection")
 	}
+
+	
 	if err := sqlDB.Close(); err != nil{
 		log.Fatal("Failed to close db connection")
 	}
